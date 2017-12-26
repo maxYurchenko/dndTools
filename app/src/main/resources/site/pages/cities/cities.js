1c883a56d4f994e469fd9a66de764e94cd4d35ce
Max Yurchenko <mvy@norse.digital>
2017-12-26 18:06:59 +0200

    city generation improvement

--- app/src/main/resources/site/pages/cities/cities.js
+++ app/src/main/resources/site/pages/cities/cities.js
@@ -11,6 +11,7 @@ exports.get = handleGet;
 
 function handleGet(req) {
     var me = this;
+    var content = portal.getContent();
 
     function renderView() {
         var view = resolve('cities.html');
@@ -27,16 +28,7 @@ function handleGet(req) {
     function createModel() {
 
         var up = req.params;
-        var content = portal.getContent();
         var response = [];
-        var cities = contentLib.query({
-            query: "_parentPath LIKE '/content" + content._path + "*'",
-            start: 0,
-            count: 10,
-            contentTypes: [
-                app.name + ":town"
-            ],
-        }).hits;
 
         if( up.generateCity ){
             generateCity( up );
@@ -48,9 +40,22 @@ function handleGet(req) {
             });
         }
 
+        // TODO: create a function to fetch all available cities(refactoring)
+        // Should look like:
+        // var cities = functionName(params);
+        var cities = contentLib.query({
+            query: "_parentPath LIKE '/content" + content._path + "*'",
+            start: 0,
+            count: 10,
+            contentTypes: [
+                app.name + ":town"
+            ],
+        }).hits;
+
         for( var i = 0; i < cities.length; i++ ){
             cities[i] = cityLib.beautifyCity(cities[i], "max(1280)", 2);
         }
+        // END TODO
 
         var model = {
             pageComponents: helpers.getPageComponents( req ),
@@ -66,29 +71,196 @@ function handleGet(req) {
     }
 
     function generateCity( params ){
-        if(isNaN( parseInt(params.human))){
-            params.human = 0;
+        params = generateParams(params);
+        var population = generatePopulation( params.size );
+        var wealth = generateWealth( population, params.size );
+        var buildings = generateBuildings(population, params.size);
+        var citizens = generateTownCitizens( population, params );
+        return ;
+    }
+
+    function generatePopulation( size ){
+        switch (size) {
+            case '0':
+                var population = Math.floor((Math.random() * 480) + 20);
+                break;
+            case '1':
+                var population = Math.floor((Math.random() * 500) + 500);
+                break;
+            case '2':
+                var population = Math.floor((Math.random() * 2000) + 1000);
+                break;
+            case '3':
+                var population = Math.floor((Math.random() * 5000) + 3000);
+                break;
+            case '4':
+                var population = Math.floor((Math.random() * 7000) + 5000);
+                break;
+            case '5':
+                var population = Math.floor((Math.random() * 9000) + 7000);
+                break;
+            case '6':
+                var population = Math.floor((Math.random() * 10000) + 10000);
+                break;
+            case '7':
+                var population = Math.floor((Math.random() * 30000) + 20000);
+                break;
+            default:
+                var population = Math.floor((Math.random() * 500) + 500);
+                break;
+            }
+        return population;
+    }
+
+    function generateWealth( population, size ){
+        return population * Math.floor((Math.random() * (100 * (size + 1))) + 10);
+    }
+
+    function generateBuildings( population, size ){
+        var buildings = {};
+        var score = {};
+        var cityHelper = contentLib.get({ key: content.data.cityHelper }).data.townBuildingType;
+        for( var i = 0; i < cityHelper.length; i++ ){
+            if( (size - i + (Math.ceil(i/2))) > 0 ){
+                var sizeCoefficient = size - i + Math.ceil(i/2);
+            } else {
+                var sizeCoefficient = 0;
+            }
+            score[cityHelper[i].type] = Math.floor((Math.random() * ((sizeCoefficient + 3) * 2)) + (sizeCoefficient * 50));
+            buildings[cityHelper[i].type] = {};
+            for( var j = 0; j < cityHelper[i].subTypes.length; j++ ){
+                buildings[cityHelper[i].type][cityHelper[i].subTypes[j].subTypeName] = 0;
+            }
         }
-        if(isNaN( parseInt(params.halfelf))){
-            params.halfelf = 0;
+
+        for( var i = 0; i < cityHelper.length; i++ ){
+            for( var j = 0; j < score[cityHelper[i].type] * 2; j++ ){
+                var subTypeIndex = Math.floor(Math.random() * cityHelper[i].subTypes.length);
+                if( parseInt(cityHelper[i].subTypes[subTypeIndex].subTypePossibility) <= score[cityHelper[i].type] ){
+                    buildings[cityHelper[i].type][cityHelper[i].subTypes[subTypeIndex].subTypeName] += 1;
+                    score[cityHelper[i].type] = score[cityHelper[i].type] - parseInt(cityHelper[i].subTypes[subTypeIndex].subTypePossibility);
+                }
+            }
         }
-        if(isNaN( parseInt(params.halforc))){
-            params.halforc = 0;
+        return buildings;
+    }
+
+    function generateTownCitizens( population, params ){
+        var citizensPossibility = {
+            elf: params.elf,
+            dwarf: params.dwarf,
+            gnome: params.gnome,
+            halfelf: params.halfelf,
+            halfling: params.halfling,
+            halforc: params.halforc,
+            human: params.human
         }
-        if(isNaN( parseInt(params.halfling))){
-            params.halfling = 0;
+        norseUtils.log(citizensPossibility);
+        for( var i = 0; i < 10; i++ ){
+            //generateSingleCitizen(Math.floor(Math.random() * 10));
         }
-        if(isNaN( parseInt(params.dwarf))){
-            params.dwarf = 0;
+    }
+
+    function getRace( params ){
+        var currRace = Math.floor(Math.random() * 101);
+    }
+
+    function generateSingleCitizen( race ){
+        var namesLocations = portal.getSiteConfig().namesLocations;
+        var useNames = selectNameArr( race );
+        var names = contentLib.get({key: namesLocations[useNames]});
+        var sex = Math.floor(Math.random() * 2);
+        return generateCitizenName(names);
+    }
+
+    function selectNameArr( race, sex ){
+        var result = '';
+        switch (race){
+            case 0:
+                // human
+                result = 'human';
+                break;
+            case 1:
+                // elf
+                result = 'elves';
+                break;
+            case 2:
+                // dwarf
+                result = 'dwarves';
+                break;
+            case 3:
+                // orc
+                result = 'orcs';
+                break;
+            case 4:
+                // halfling
+                result = 'human';
+                break;
+            case 5:
+                // gnome
+                result = 'human';
+                break;
+            case 6:
+                // half-elf
+                result = 'elves';
+                break;
+            case 7:
+                // dragonborn
+                result = 'other';
+                break;
+            case 8:
+                // aarakocra
+                result = 'other';
+                break;
+            case 9:
+                // tiefling
+                result = 'other';
+                break;
+            case 10:
+                // goliath
+                result = 'other';
+                break;
+            default:
+                break;
         }
-        if(isNaN( parseInt(params.gnome))){
-            params.gnome = 0;
+        if( result == 'other' ){
+            return result;
         }
-        if(isNaN( parseInt(params.elf))){
-            params.elf = 0;
+        switch (sex){
+            case 0:
+                result += 'Male';
+                break;
+            default:
+                result += 'Female';
+                break;
         }
-        if( ((parseInt(params.human) + parseInt(params.halfelf) + parseInt(params.halfling) + parseInt(params.halforc) +
-            parseInt(params.dwarf) + parseInt(params.gnome) + parseInt(params.elf)) == 100) ) {
+        return result;
+    }
+
+    function generateCitizenName( nameArr ){
+        var namesArrId = ['first', 'second', 'third', 'fourth'];
+        var firstName = '';
+        var lastName = '';
+        for( var i = 0; i < namesArrId.length; i++ ){
+            var canBreak = true;
+            if( nameArr.data && nameArr.data.firstName && nameArr.data && nameArr.data.firstName && nameArr.data.firstName[namesArrId[i]] ){
+                firstName += nameArr.data.firstName.first[Math.floor(Math.random() * nameArr.data.firstName.first.length)];
+                canBreak = false;
+            }
+            if( nameArr.data && nameArr.data.lastName && nameArr.data && nameArr.data.lastName && nameArr.data.lastName[namesArrId[i]] ){
+                lastName += nameArr.data.lastName[namesArrId[i]][Math.floor(Math.random() * nameArr.data.lastName[namesArrId[i]].length)];
+                canBreak = false;
+            }
+            if( canBreak ){
+                break;
+            }
+        }
+        return firstName + ' ' + lastName;
+    }
+
+    function generateParams( params ){
+        params = checkParams(params);
+        if( ((params.human + params.halfelf + params.halfling + params.halforc + params.dwarf + params.gnome + params.elf) == 100) ) {
             var cityParams = {
                 name: params.name,
                 size: params.size,
@@ -106,21 +278,65 @@ function handleGet(req) {
                 agricultural: params.agricultural,
                 forest: params.forest
             };
-        }
-        else{
+        } else {
             var cityParams = {
                 name: params.name,
                 size: params.size,
-                dwarf: '8',
-                elf: '7',
-                gnome: '5',
-                halfelf: '5',
-                halfling: '10',
-                halforc: '5',
-                human: '60',
+                dwarf: 8,
+                elf: 7,
+                gnome: 5,
+                halfelf: 5,
+                halfling: 10,
+                halforc: 5,
+                human: 60,
                 children: ''
             };
         }
+        return cityParams;
+    }
+
+    function checkParams( params ){
+        if(isNaN( parseInt(params.human))){
+            params.human = 0;
+        } else {
+            params.human = parseInt(params.human);
+        }
+        if(isNaN( parseInt(params.halfelf))){
+            params.halfelf = 0;
+        } else {
+            params.halfelf = parseInt(params.halfelf);
+        }
+        if(isNaN( parseInt(params.halforc))){
+            params.halforc = 0;
+        } else {
+            params.halforc = parseInt(params.halforc);
+        }
+        if(isNaN( parseInt(params.halfling))){
+            params.halfling = 0;
+        } else {
+            params.halfling = parseInt(params.halfling);
+        }
+        if(isNaN( parseInt(params.dwarf))){
+            params.dwarf = 0;
+        } else {
+            params.dwarf = parseInt(params.dwarf);
+        }
+        if(isNaN( parseInt(params.gnome))){
+            params.gnome = 0;
+        } else {
+            params.gnome = parseInt(params.gnome);
+        }
+        if(isNaN( parseInt(params.elf))){
+            params.elf = 0;
+        } else {
+            params.elf = parseInt(params.elf);
+        }
+        if(isNaN( parseInt(params.size))){
+            params.size = 0;
+        } else {
+            params.size = parseInt(params.size);
+        }
+        return params;
     }
 
     return renderView();
--- app/src/main/resources/site/pages/nameArray/nameArray.js
+++ app/src/main/resources/site/pages/nameArray/nameArray.js
@@ -8,6 +8,9 @@ var helpers = require('helpers');
 
 exports.get = handleGet;
 
+//var names = ["Arjhan","Balasar","Bharash","Donaar","Ghesh","Heskan","Kriv","Medrash","Mehen","Nadarr","Pandjed","Patrin","Rhogar","Shamash","Shedinn","Tarhun","Torinn","Akra","Biri","Daar","Farideh","Harann","Havilar","Jheri","Kava","Korinn","Mishann","Nala","Perra","Raiann","Sora","Surina","Thava","Uadjit","Aera","Aial","Aur","Deekek","Errk","Heehk","Ikki","Kleeck","Oorr","Ouss","Quaf","Quierk","Salleek","Urreek","or Zeed","Akmenos","Amnon","Barakas","Damakos","Ekemon","Iados","Kairon","Leucis","Melech","Mordai","Morthos","Pelaios","Skamos","Therai","Akta","Anakis","Bryseis","Criella","Damaia","Ea","Kallista","Lerissa","Makaria","Nemeia","Orianna","Phelaia","Rieta","Aukan","Eglath","Gae-Al","Gauthak","Ilikan","Keothi","Kuori","Lo-Kag","Manneo","Maveith","Nalla","Orilo","Paavu","Pethani","Thalai","Thotham","Uthal","Vaunea","Vimak"];
+//var names2 = ["Anakalathai","Elanithino","Gathakanathi","Kalagiano","Katho-Olavi","Kolae-Gileana","Ogolakanu","Thuliaga","Thunukalathi","Vaimei-Laga","Art","Carrion","Chant","Creed","Despair","Excellence","Fear","Glory","Hope","Ideal","Music","Nowhere","Open","Poetry","Quest","Random","Reverence","Sorrow","Temerity","Torment","Weary","Clethtinthiallor","Daardendrian","Delmirev","Drachedandion","Fenkenkabradon","Kepeshkmolik","Kerrhylon","Kimbatuul","Linxakasendalor","Myastan","Nemmonis","Norixius","Ophinshtalajiir","Prexijandilin","Shestendeliath","Turnuroth","Verthisathurgiesh","Yarjerit"];
+
 function handleGet(req) {
     var me = this;
 
@@ -26,6 +29,10 @@ function handleGet(req) {
         var up = req.params;
         var content = portal.getContent();
         var response = [];
+        /*var result = contentLib.modify({
+            key: content._id,
+            editor: editor
+        });*/
 
         var model = {
             pageComponents: helpers.getPageComponents( req ),
@@ -37,6 +44,12 @@ function handleGet(req) {
 
 
     }
+
+    /*function editor(c) {
+        c.data.firstName.first = names;
+        c.data.lastName.first = names;
+        return c;
+    }*/
 
     return renderView();
 }
